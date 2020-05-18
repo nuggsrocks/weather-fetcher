609b4ace4aa618b8b70a2a290d74eabf9a77cf3b
mattfisher <nuggsrocks@yahoo.com>
2020-05-18 17:47:23 -0400

    nginx proxy server now working

--- back-end/server.js
+++ back-end/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-app.route('/weather-fetcher/server').get((req, res) => {
+app.route('/').get((req, res) => {
     fetchWeather(req.query.location, res);
 });
 
